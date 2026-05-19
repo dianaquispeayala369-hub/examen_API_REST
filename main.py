731c3d38b84976247cfbdd5d798b5ae151bd0581
dianaquispeayala369-hub <dianaquispeayala369@gmail.com>
2026-05-19 11:17:26 -0500

    add main.py

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from flask import Flask, # FLASH, redirect, render_template, request, session, abort
 
-app= Flask(__name__)
+from flask import Flask, render_template
+from database.connection import close_db_connection, init_db
+from routes.students import students_bp
+import os
 
-def hello():
-    print("Hello, World!")
+app = Flask(__name__)
+
+# Conectamos las rutas del examen
+app.register_blueprint(students_bp)
+
+# Cerramos las conexiones de base de datos automáticamente al terminar cada petición
+app.teardown_appcontext(close_db_connection)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    if not os.path.exists("students.db"):
+        init_db(app)
+        print("📁 Base de datos 'students.db' creada e inicializada.")
+        
+    app.run(debug=True, port=5000)
